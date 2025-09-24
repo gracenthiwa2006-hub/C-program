d94375ad69310b4c58dff6d2d6d34dc628e911c7
gracenthiwa2006-hub <gracenthiwa2006@gmail.com>
2025-09-24 17:53:20 +0300

    Add files via upload

---
+++ c program displaying my units.cpp
@@ -0,0 +1,21 @@
+//simple c program
+/*
+name:Grace Nthiwa
+reg no:CT100/G/26265/25
+description:units that am taking
+*/
+
+#include<stdio.h>
+
+//main function
+int main(){
+	printf("Analogue electronics \n");
+	printf("Introduction to programing & algorithms \n");
+	printf("Computer systems & organizations \n");
+	printf("Introduction to computer application \n");
+	printf("Essentials of accounting \n");
+	printf("Mathematics for science \n");
+	printf("Fundamentals of ICT \n");
+	printf("Communication & information literacy skills \n");
+	return 0;
+}
